29c48043a4632c75fb9d75e4864d2057065b7c90
SANDWIP MITRA <43679584+sandwipmitra@users.noreply.github.com>
2024-04-21 03:40:14 +0000

    3rdcommit

--- main.py
+++ main.py
@@ -9,7 +9,37 @@ for word in book_title:
   else:
     word_counter[word] = word_counter[word] + 1
 
-print(word_counter)
+# print(word_counter)
+# for word in book_title:
+#   if word not in word_counter:
+#     word_counter[word] = 1
+#   else:
+#     word_counter[word] += 1
+
+# print(word_counter)
+
 
 
 #Method 2: Using the get method
+for word in book_title:
+  word_counter[word] = word_counter.get(word, 0) + 1
+#Syntax
+#dictionary.get(keyname, value)
+
+# print(word_counter)
+
+#more examples
+cast = {
+     "Jerry Seinfeld": "Jerry Seinfeld",
+     "Julia Louis-Dreyfus": "Elaine Benes",
+     "Jason Alexander": "George Costanza",
+     "Michael Richards": "Cosmo Kramer",
+      "Michael Richards": "Cosmo Kramer"
+ }
+print("Iterating through keys:")
+for key in cast:
+    print(key)
+
+print("\nIterating through keys and values:")
+for key, value in cast.items():
+  print("{}, {}".format(key, value))
